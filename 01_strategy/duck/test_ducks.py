d770f908d27cf4b2f76008337eb521b15df2852e
denzow <denzow@gmail.com>
2018-01-09 23:57:21 +0900

    [wip] kamokamo.

---
+++ 01_strategy/duck/test_ducks.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+from unittest import TestCase
+from lib.ducks import Duck
+
+
+class TestDucks(TestCase):
+
+    def test_perform_quack(self):
+        duck = Duck()
+
+
+    def test_perform_fly(self):
+        self.fail()
+
